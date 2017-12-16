48974f65b2fff7b44a5251e4e41a44a0be2cd4dc
AmolVagad <amolvagad@gmail.com>
2017-12-16 17:41:19 -0600

    File renaming and added global file with elimination of unnecessary functions

--- Adam_ICP.cpp
+++ Adam_ICP.cpp
@@ -6,90 +6,53 @@
 #include<time.h>
 #include<sys/time.h>
 #include<ctime>
-#include "dlib/optimization/optimization.h"
-#include "dlib/optimization/find_optimal_parameters_abstract.h"
-#include "dlib/optimization/optimization_bobyqa.h"
-#include "dlib/optimization/find_optimal_parameters.h"
 
+#include"globals.h"
 using namespace std;
 using namespace dlib;
 
 
-typedef dlib::matrix<double,0,1> column_vector;
+///////////////////////////// Function declarations///////////////////////////////////
 
-struct point_cloud_data{
+//Function to calculate the point at shortest distance 
+void cal_closest_points(const column_vector &rt);
+// Function to calculate gradient of cost function
+column_vector CalculateGradient(const column_vector rt, column_vector gradient);
+// Function that uses Adam optimizer for optimization
+column_vector AdamOptimizer(const column_vector rt);
+//Function to calculate norm
+double cal_norm(column_vector r);
+//Function to calculate elementwise division 
+dlib::matrix<double> pointwise_divide(dlib::matrix<double> m, dlib::matrix<double> v);
+// Function to compute rotation of the data point
+dlib::matrix<double> PerformRotation(dlib::matrix<double> R,dlib::matrix<double> t, dlib::matrix<double> point)
 
-	std::vector <double> x_coord;
-	std::vector <double> y_coord;
-	std::vector <double> z_coord;
-	std::vector <int> index;
-	int size ;	
-};
+///////////////////////////////////////////////////////////////////////////////
+
+
+
+
+//////////////////Initializations/////////////////////////////////
 
 point_cloud_data measurement_data;
 point_cloud_data model_data;
 point_cloud_data transformed_data;
-//point_cloud_data transformed_data;
-int iterations = 0;
-int skips = 5000;
 
 
-//dlib::matrix<double> point(3,1);
-//dlib::matrix<double> Rot_point(3,1);
 
 
-void cal_closest_points(const column_vector &rt);
-
-double findTotalErrorInCloud(const column_vector &rt);
-
-column_vector CalculateGradient(const column_vector rt, column_vector gradient);
-
-column_vector AdamOptimizer(const column_vector rt);
-
-double cal_norm(column_vector r);
-
-dlib::matrix<double> pointwise_divide(dlib::matrix<double> m, dlib::matrix<double> v);
-
-dlib::matrix<double> PerformRotation(dlib::matrix<double> R,dlib::matrix<double> t, dlib::matrix<double> point)
-{
-	dlib::matrix<double> point_new(3,1);
-	point_new = R*point + t;
-	return point_new;
-}
-
-void PerformTransformationToAllPoints(dlib::matrix<double> R, dlib::matrix<double> t, point_cloud_data * data, point_cloud_data * transformed_data, int skips)
-{
-	for(int i  = 0; i < data->size; i++)
-	{
-		//cout<<"Print x "<<data->x_coord.at(i)<<endl;
-		dlib::matrix<double,3,1> point, point_new;
-		point = data->x_coord.at(i), data->y_coord.at(i), data->z_coord.at(i);
-		//cout<<"Error here above "<<endl;
-		point_new = PerformRotation(R, t, point);
-		transformed_data->x_coord.push_back(point_new(0));
-		transformed_data->y_coord.push_back(point_new(1));
-		transformed_data->z_coord.push_back(point_new(2));		 
-	}
-	transformed_data->size = transformed_data->x_coord.size();
-}
-
+/////////////// Main function////////////////////////////////////////////////////////////
 int main()
 {
 	
 
-	
+	// Load model data from .csv file that contains 3D point cloud data	
 	
 	ifstream infile1;
   	infile1.open ("icp_model.csv");
-	//ifstream infile2;
-  	//infile2.open ("icp_sensor_scan.csv");
+	
 	char* pEnd;
 	string x,y,z;
-
-	
-
-	// Reading data from the model map data csv file 
-
 
 	 while(!infile1.eof()){
 		getline(infile1,x, ',');
@@ -123,17 +86,14 @@ int main()
 
 	t = point_x, point_y, point_z;
 	
-	//R theta = -0.785398
-	//x = -0.014142135
-	//y = 0
-	//z = 0
 
-	// Generate mesasurement datra by rorating the model data
+
+	// Generate mesasurement data by rotating the model data
 	PerformTransformationToAllPoints(R, t, &model_data, &measurement_data,1);
 
 	cout<<"Measurement data size "<<measurement_data.size<<endl;
 
-	//Calling closest point. Currently for testing purpose I am matching i and j
+	//Calculating the closest distance for a point 
 	column_vector rt(4), rt_lower(4), rt_upper(4), gradient(4);
 
 	rt = -theta, -cos(theta)*point_x - sin(theta)*point_y, sin(theta)*point_x - cos(theta)*point_y, point_z;
@@ -151,11 +111,11 @@ int main()
 	{
 		cout<<"iteration #: "<<i<<endl;
 		cpu_starttime = clock();
-		cal_closest_points(rt);
+		cal_closest_points(rt);    // Function call to find the shortest distance
 		cpu_endtime = clock();
 		cout<<"The time taken for calculation = "<<((cpu_endtime - cpu_starttime)/CLOCKS_PER_SEC)<<endl;
-		rt = AdamOptimizer(rt);
-		//final_error = find_optimal_parameters(0.01, 0.000000001,100000, rt, rt_lower, rt_upper,findTotalErrorInCloud);
+		rt = AdamOptimizer(rt);   // Function call to optimizer function
+
 		cout<<"Rt parameters "<<rt<<endl;
 		
 		
@@ -168,6 +128,15 @@ int main()
 
 	return 0;
 }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
 
 void cal_closest_points(const column_vector &rt)
 {
@@ -220,48 +189,6 @@ void cal_closest_points(const column_vector &rt)
 
 }
 
-
-
-double findTotalErrorInCloud(const column_vector &rt)
-{
-	
-	double icp_error = 0.0;
-	//point_cloud_data transformed_data;
-	
-	dlib::matrix<double> R(3,3);
-	dlib::matrix<double> t(3,1);
-
-	R = cos(rt(0)), -sin(rt(0)), 0,
-	    sin(rt(0)), cos(rt(0)), 0,
-	    0, 0, 1;
-
-	t = rt(1), rt(2), rt(3);
-	//cout<<"Check measurement data element "<<measurement_data.x_coord.at(0)<<endl;
-	transformed_data.x_coord.clear();
-	transformed_data.y_coord.clear();
-	transformed_data.z_coord.clear();
-	transformed_data.index.clear();
-	//cout<<"Transformed data before "<<transformed_data.size<<endl;
-	PerformTransformationToAllPoints(R, t, &measurement_data, &transformed_data,1);
-	//cout<<"Transformed data after "<<transformed_data.size<<endl;
-	//cout<<"Check 1"<<endl;
-	double true_map_error = 0.0;
-	for(int i = 0; i < measurement_data.size; i++)
-	{
-		//cout<<"Value of i "<<i<<endl;
-		int j = measurement_data.index.at(i);
-
-		icp_error +=(pow((transformed_data.x_coord.at(i) - model_data.x_coord.at(j)),2) + pow((transformed_data.y_coord.at(i) - model_data.y_coord.at(j)),2) + pow((transformed_data.z_coord.at(i) - model_data.z_coord.at(j)),2)); 
-
-		//true_map_error +=sqrt(pow((transformed_data.x_coord.at(i) - model_data.x_coord.at(i)),2) + pow((transformed_data.y_coord.at(i) - model_data.y_coord.at(i)),2) + pow((transformed_data.z_coord.at(i) - model_data.z_coord.at(i)),2));
-
-	}
-
-
-	//cout<<"current error: "<<total_error<<endl;
-	//cout<<"final map error: "<<true_map_error<<endl;
-	return icp_error;
-}
 
 column_vector CalculateGradient(column_vector rt, column_vector gradient)
 {
